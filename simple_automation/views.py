87f28a52aa6307fe0c510175479b6fb427e3ce7c
Uzuran <archloverlinux@gmail.com>
2024-06-29 11:50:55 +0200

    - Create a zip file to hold the modified files for future download.

--- simple_automation/views.py
+++ simple_automation/views.py
@@ -1,11 +1,15 @@
 """Views for simple app"""
+import os
+import zipfile
+
 import django.contrib.auth
 from datetime import datetime
-
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
+from my_portfolio_web import settings
 from simple_automation.forms import SignupForm, LoginForm, UploadFileForm
+from django.shortcuts import render
+import io
 
 
 def user_register_view(response):
@@ -62,12 +66,32 @@ def user_check(request):
 
 
 def upload_file(request):
-
     if request.method == 'POST':
-        file = request.FILES.getlist('file')
-        file_names = [f.name for f in file]
-        # Here we can work with the files use basic python scripts.
-        return HttpResponse(f"This is file name:{','.join(file_names)}")
+        files = request.FILES.getlist('file')
+        response = HttpResponse(content_type='application/zip')
+        response['Content-Disposition'] = 'attachment; filename="modified_files.zip"'
+
+        # Create a zip file to hold the modified files
+        with io.BytesIO() as zip_buffer:
+            with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+                for uploaded_file in files:
+                    # Check if the file ends with .txt
+                    if uploaded_file.name.endswith('.txt'):
+                        # Read the uploaded file
+                        file_content = uploaded_file.read().decode('utf-8')
+
+                        # Replace '1.4301-4.0' with '1.4301'
+                        new_content = file_content.replace('1.4301-4.0', '1.4301')
+
+                        # Add the modified file to the zip
+                        zip_file.writestr(uploaded_file.name, new_content)
+
+            # Set the zip buffer's position to the beginning
+            zip_buffer.seek(0)
+            response.write(zip_buffer.read())
+
+        return response
     else:
         form = UploadFileForm()
-    return render(request, 'main_work_screen.html', {'form': form})
+        return render(request, 'main_work_screen.html', {'form': form})
+
