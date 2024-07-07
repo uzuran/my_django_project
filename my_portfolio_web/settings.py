88cbb0a084376d1102c9f2b29d3ed5814ed94b58
Uzuran <archloverlinux@gmail.com>
2024-07-07 15:03:10 +0200

    - Change ods_file_patch

--- my_portfolio_web/settings.py
+++ my_portfolio_web/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),  # Optional: for global/static files
 ]
 
 # Quick-start development settings - unsuitable for production
--- my_portfolio_web_app/static_root/js/clock.js
+++
@@ -1,8 +0,0 @@
-function updateTime() {
-    const now = new Date();
-    const timeString = now.toLocaleTimeString();
-    const dateString = now.toLocaleDateString();
-    document.getElementById('time').textContent = timeString;
-    document.getElementById('date').textContent = dateString;
-}
-setInterval(updateTime, 1000);
